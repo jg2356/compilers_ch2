9ae9923290f271828fa141103f22de9b6c92731b
Jose Gomez <k13gomez@gmail.com>
2015-05-17 09:49:51 -0400

    more responses

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,12 @@
 public class Runner {
 	public static void run(IO io, Parser parser) {
 		io.out("Press Ctrl+C or enter a blank line to exit.\r\n\r\nEnter an expression to parse according to the grammar:\r\n" + parser.grammar() + "\r\n");
+		
 		while (true) {
 			io.out("> ");
 			String expression = io.in();
 			
-			if (expression.length() == 0)
+			if (expression == null || expression.length() == 0)
 				break;
 			
 			Input input = new Input(expression);
